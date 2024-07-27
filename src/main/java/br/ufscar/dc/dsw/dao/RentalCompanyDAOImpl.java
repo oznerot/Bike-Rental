dcfdb8f0995f459e604d0ee2f7791bfffe0e4c6d
oznerot <vitorenzocs@gmail.com>
2024-07-27 20:02:25 -0300

    RentalCompantDAOImpl() methods need testing

--- src/main/java/br/ufscar/dc/dsw/dao/RentalCompanyDAOImpl.java
+++ src/main/java/br/ufscar/dc/dsw/dao/RentalCompanyDAOImpl.java
@@ -22,10 +22,11 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
     public RentalCompany get(String uuid)
     {
         RentalCompany company = null;
-        String sql = "SELECT uuid, name, email, password, cnpj, city FROM rental_company WHERE uuid = ?";
-
-        //Here I'm using try-with-resources statement to let the Automatic Resource Management close
-        //Connection, preparedStatement and ResultSet objects for me so I don't mess things up.
+        String sql = "SELECT rc.*, u.name, u.email, u.password " +
+                     "FROM rental_company rc " +
+                     "JOIN user u ON rc.company_id = u.user_id " +
+                     "WHERE company_id = ?"
+        
         try(Connection conn = Database.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql))
         {
@@ -35,7 +36,7 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
             {
                 if(rs.next())
                 {
-                    String companyUUID = rs.getString("uuid");
+                    String companyUUID = rs.getString("company_id");
                     String companyName = rs.getString("name");
                     String companyEmail = rs.getString("email");
                     String companyPassword = rs.getString("password");
@@ -43,7 +44,6 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
                     String companyCity = rs.getString("city");
 
                     company = new RentalCompany(companyUUID, companyName, companyEmail, companyPassword, companyCnpj, companyCity);
-
                 }
             }
         }
@@ -58,17 +58,21 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
     public List<RentalCompany> getByCity(String city)
     {
         List<RentalCompany> companyList = new ArrayList<>();
-        String sql = "SELECT uuid, name, email, password, cnpj, city FROM rental_company WHERE city = ?";
-
+        String sql = "SELECT rc.*, u.name u.email, u.password " +
+                     "FROM rental_company rc " +
+                     "JOIN user u ON rc.company_id = u.user_id" +
+                     "WHERE city = ?"
+        
         try(Connection conn = Database.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql))
         {
             ps.setString(1, city);
+
             try(ResultSet rs = ps.executeQuery())
             {
                 while(rs.next())
                 {
-                    String companyUUID = rs.getString("uuid");
+                    String companyUUID = rs.getString("company_id");
                     String companyName = rs.getString("name");
                     String companyEmail = rs.getString("email");
                     String companyPassword = rs.getString("password");
@@ -93,21 +97,23 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
     public List<RentalCompany> getAll()
     {
         List<RentalCompany> companyList = new ArrayList<>();
-        String sql = "SELECT uuid, name, email, password, cnpj, city FROM rental_company";
-
+        String sql = "SELECT rc.*, u.name u.email, u.password " +
+                     "FROM rental_company rc " +
+                     "JOIN user u ON rc.company_id = u.user_id";
+        
         try(Connection conn = Database.getConnection();
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql))
         {
             while(rs.next())
             {
-                String companyUUID = rs.getString("uuid");
+                String companyUUID = rs.getString("company_id");
                 String companyName = rs.getString("name");
                 String companyEmail = rs.getString("email");
                 String companyPassword = rs.getString("password");
                 String companyCnpj = rs.getString("cnpj");
                 String companyCity = rs.getString("city");
-                
+
                 RentalCompany company = new RentalCompany(companyUUID, companyName, companyEmail, companyPassword, companyCnpj, companyCity);
 
                 companyList.add(company);
@@ -117,7 +123,6 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
         {
             e.printStackTrace();
         }
-
         return companyList;
     }
 
@@ -125,19 +130,48 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
     public int insert(RentalCompany company)
     {
         int result = 0;
-        String sql = "INSERT INTO rental_company (uuid, name, email, password, cnpj, city) VALUES (?, ?, ?, ?, ?, ?)";
-
-        try(Connection conn = Database.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql))
+        String userSQL = "INSERT INTO user (user_id, name, email, password, user_role) VALUES " +
+                         "(?, ?, ?, ?, (SELECT role_id FROM roles WHERE role_name = 'rental_company'))";
+        
+        String companySQL = "INSERT INTO rental_company (company_id, cnpj, city) VALUES " +
+                            "(?, ?, ?)";
+        
+        try(Connection conn = Database.getConnection())
         {
-            ps.setString(1, company.getUUID());
-            ps.setString(2, company.getName());
-            ps.setString(3, company.getEmail());
-            ps.setString(4, company.getPassword());
-            ps.setString(5, company.getCnpj());
-            ps.setString(6, company.getCity());
+            conn.setAutoCommit(false);
 
-            result = ps.executeUpdate();
+            try(PreparedStatement userPstmt = conn.prepareStatement(userSQL);
+                PreparedStatement companyPstmt = conn.prepareStatement(companySQL))
+            {
+                userPstmt.setString(1, company.getUUID());
+                userPstmt.setString(2, company.getName());
+                userPstmt.setString(3, company.getEmail());
+                userPstmt.setString(4, company.getPassword());
+                result = userPstmt.executeUpdate();
+
+                companyPstmt.setString(1, company.getUUID());
+                companyPstmt.setString(2, company.getCnpj());
+                companyPstmt.setString(3, company.getCity());
+                result = companyPstmt.executeUpdate();
+
+                conn.commit();
+            }
+            catch(SQLException e)
+            {
+                if(conn != null)
+                {
+                    try
+                    {
+                        conn.rollback();
+                    }
+                    catch(SQLException rollbackEx)
+                    {
+                        rollbackEx.printStackTrace();
+                    }
+                }
+
+                e.printStackTrace();
+            }
         }
         catch(SQLException e)
         {
@@ -151,25 +185,54 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
     public int update(RentalCompany company)
     {
         int result = 0;
-        String sql = "UPDATE rental_company SET name = ?, email = ?, password = ?, cnpj = ?, city = ? WHERE uuid = ?";
-
-        try(Connection conn = Database.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql))
+        String userSQL = "UPDATE user SET name = ?, email = ?, password = ? " +
+                         "WHERE user_id = ?";
+        
+        String companySQL = "UPDATE company SET cnpj = ?, city = ? " +
+                            "WHERE company_id = ?";
+        
+        try(Connection conn = Database.getConnection())
         {
-            ps.setString(1, company.getName());
-            ps.setString(2, company.getEmail());
-            ps.setString(3, company.getPassword());
-            ps.setString(4, company.getCnpj());
-            ps.setString(5, company.getCity());
-            ps.setString(6, company.getUUID());
+            conn.setAutoCommit(false);
 
-            result = ps.executeUpdate();
+            try(PreparedStatement userPstmt = conn.prepareStatement(userSQL);
+                PreparedStatement companySQL = conn.prepareStatement(companySQL))
+            {
+                userPstmt.setString(1, company.getName());
+                userPstmt.setString(2, company.getEmail());
+                userPstmt.setString(3, company.getPassword());
+                userPstmt.setString(4, company.getUUID());
+                result = userPstmt.executeUpdate(); 
+
+                companyPstmt.setString(1, company.getCnpj());
+                companyPstmt.setString(2, company.getCity());
+                companyPstmt.setString(3, company.getUUID());
+                result = clientPstmt.executeUpdate();
+
+                conn.commit();
+            }
+            catch(SQLException e)
+            {
+                if(conn != null)
+                {
+                    try
+                    {
+                        conn.rollback();
+                    }
+                    catch(SQLException rollbackEx)
+                    {
+                        rollbackEx.printStackTrace();
+                    }
+                }
+
+                e.printStackTrace();
+            }
         }
         catch(SQLException e)
         {
             e.printStackTrace();
         }
-
+        
         return result;
     }
 
@@ -177,7 +240,7 @@ public class RentalCompanyDAOImpl implements RentalCompanyDAO
     public int delete(RentalCompany company)
     {
         int result = 0;
-        String sql = "DELETE FROM rental_company WHERE uuid = ?";
+        String sql = "DELETE FROM user WHERE user_id = ?";
 
         try(Connection conn = Database.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql))
